7428f94abba7366fd68f19445e5370cf41cc4171
Damith Karunaratne <damithuom@gmail.com>
2023-07-12 16:48:41 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$obj = new \Jerome\App\ClassName();
+$obj->test();
---
+++ src/ClassName.php
@@ -0,0 +1,8 @@
+<?php
+namespace Jerome\App;
+
+class ClassName {
+    public function test() {
+        echo "Hello World!";
+    }
+}
